40f170ab0b1a91044c9b6f04b867a4bc662931c0
Elliot Hatch <elliot.hatch@gmail.com>
2016-01-18 22:04:26 -0700

    Allow CORS on API routes

--- server/routes.js
+++ server/routes.js
@@ -3,6 +3,12 @@ var router = express.Router();
 
 var solsticeJamRoutes = require('./solsticeJam/routes');
 
+// allow CORS for the api routes
+router.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 router.use('/winter-jam-2015', solsticeJamRoutes('winter-jam-2015'));
 router.use(function(err, req, res, next) {
 	if(!err.status || err.status >= 500) {
